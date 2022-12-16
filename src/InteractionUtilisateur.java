1895a95b1f7b1febea1c069a8240f4418c2df9c1
OleksandrYushchenko <oleksandr.yuschenko@le-campus-numerique.fr>
2022-12-16 12:59:20 +0100

    try catch for get coordinates added

--- src/InteractionUtilisateur.java
+++ src/InteractionUtilisateur.java
@@ -26,25 +26,33 @@ public class InteractionUtilisateur {
     }
     public int get_Type_Of_Game(){
         Scanner scanner = new Scanner(System.in);
-            view.get_Type_Of_Game_List();
-            view.Enter_number_of_players();
+        view.get_Type_Of_Game_List();
+        view.Enter_number_of_players();
         return scanner.nextInt();
     }
     public int[] getCoordinates(Integer size){
         Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
-        System.out.print(visualization.RED_BOLD + "Enter coordinate X - " + visualization.ANSI_RESET);
-        int x = sc.nextInt();
-        while (x > size) {
-            System.out.print(visualization.RED_BOLD + "Enter coordinate X " + "(x<=" + size + ") - " + visualization.ANSI_RESET);
-            x = sc.nextInt();
+        int x = size + 1;
+        int y = size + 1;
+        while (x > size - 1 && y > size - 1) {
+            try {
+                System.out.print(visualization.RED_BOLD + "Enter coordinate X - " + visualization.ANSI_RESET);
+                x = sc.nextInt();
+                while (x > size - 1) {
+                    System.out.print(visualization.RED_BOLD + "Enter coordinate X " + "(x<=" + size + ") - " + visualization.ANSI_RESET);
+                    x = sc.nextInt();
+                }
+                System.out.print(visualization.GREEN_BOLD + "Enter coordinate Y - " + visualization.ANSI_RESET);
+                y = sc.nextInt();
+                while (y > size - 1) {
+                    System.out.print(visualization.GREEN_BOLD + "Enter coordinate Y " + "(y<=" + size + ") - " + visualization.ANSI_RESET);
+                    y = sc.nextInt();
+                }
+            } catch (Exception e) {
+                System.out.println("Entered data is not valid");
+                sc.next();
+            }
         }
-        System.out.print(visualization.GREEN_BOLD + "Enter coordinate Y - " + visualization.ANSI_RESET);
-        int y = sc.nextInt();
-        while (y > size) {
-            System.out.print(visualization.GREEN_BOLD + "Enter coordinate Y " + "(y<=" + size + ") - " + visualization.ANSI_RESET);
-            y = sc.nextInt();
-        }
-
         return new int[]{ x, y };
     }
 }
